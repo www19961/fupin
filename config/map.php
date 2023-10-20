4ffbd412b14ed225805144f81948264cca94a367
aaa <peterstonesooo@outlook>
2023-10-20 13:21:20 +0800

    ++

--- config/map.php
+++ config/map.php
@@ -58,7 +58,7 @@ return array(
                     0 => '禁用',
                     1 => '启用',
                 ),
-            'setting_key'=>['apk_download_url','version_apk','video_url','video_img_url','kefu_url','register_domain','is_req_encypt'],
+            'setting_key'=>['apk_download_url','version_apk','video_url','video_img_url','kefu_url','register_domain','is_req_encypt','microcore_group','qq_group'],
         ),
     'project' =>
         array(
