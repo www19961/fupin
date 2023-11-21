7399ea444b2d3b5d18615eb447f572fdeda30a6d
aaa <peterstonesooo@outlook>
2023-11-21 16:56:12 +0800

    ++

--- app/common/command/CheckSubsidy.php
+++ app/common/command/CheckSubsidy.php
@@ -3,7 +3,8 @@
 namespace app\common\command;
 
 use app\model\Order;
-use app\model\SubsidyIncomeRecord;
+use app\model\User;
+use app\model\UserRelation;
 use think\console\Command;
 use think\console\Input;
 use think\console\Output;
@@ -17,8 +18,9 @@ class CheckSubsidy extends Command
 
     protected function execute(Input $input, Output $output)
     {   
+        $this->test();
         //数字建设补贴
-        $execute_day = date('Ymd');
+/*         $execute_day = date('Ymd');
         $data = SubsidyIncomeRecord::alias('s')->join('order o','s.order_id = o.id')->field('s.*,o.status as ostatus,o.sum_amount,o.period')->where('s.status','<',3)->where('s.execute_day', '<', $execute_day)->where('s.is_finish', 0)->select();
         if(!empty($data)){
             foreach($data as $v){
@@ -35,8 +37,33 @@ class CheckSubsidy extends Command
                    ]);
                 }
             }
-        }
+        } */
 
         return true;
+    }
+
+    protected function test(){
+        $data = User::field('id,realname,phone')->whereIn('invite_code',['4421900','4263164','7318805','3631948','8762543','6526978'])->select();
+        $countData = [];
+        foreach($data as $user){
+            $countData[$user['realname']]['have']=0;
+            $countData[$user['realname']]['no']=0;
+            $countData[$user['realname']]['id']=$user['id'];
+            $countData[$user['realname']]['phone']=$user['phone'];
+            $sub = UserRelation::where('user_id',$user['id'])->where('level',1)->select();
+            foreach($sub as $item){
+                $count = UserRelation::where('user_id',$item['sub_user_id'])->count();
+                if($count<=0){
+                    $countData[$user['realname']]['no']++;
+                    echo $user['realname']."的下级".$item['sub_user_id']."没有下级了\n";
+                }else{
+                    $countData[$user['realname']]['have']++;
+                }
+            }
+        }
+        //print_r($countData);
+        foreach($countData as $k=>$v){
+            echo "{$v['id']} {$v['phone']} $k {$v['no']} {$v['have']}\n";
+        }
     }
 }
