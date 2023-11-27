63f510a8e114585d389bcd5d25df7602df0d0e41
aaa <peterstonesooo@outlook>
2023-11-28 01:00:58 +0800

    ++

--- app/api/controller/SigninController.php
+++ app/api/controller/SigninController.php
@@ -32,9 +32,9 @@ class SigninController extends AuthController
 
         // 每天签到时间为8：00-20：00 早上8点到晚上21点
         $timeNum = (int)date('Hi');
-        if ($timeNum < 800 || $timeNum > 2100) {
+/*         if ($timeNum < 800 || $timeNum > 2100) {
             return out(null, 10001, '签到时间为早上8:00到晚上21:00');
-        }
+        } */
         $host = Request::host();
         if(in_array($host,$arr)){
             return out(null, 10001, '请联系客服下载最新app进行签到');
