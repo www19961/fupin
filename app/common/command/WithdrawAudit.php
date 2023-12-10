59bad4852fd392713219191623c3102051ca35d1
aaa <peterstonesooo@outlook>
2023-12-10 17:03:08 +0800

    ++

---
+++ app/common/command/WithdrawAudit.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\common\command;
+
+use app\model\Order;
+use app\model\PassiveIncomeRecord;
+use app\model\User;
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use think\facade\Db;
+use think\console\input\Argument;
+use Exception;
+use think\facade\Log;
+use app\model\Capital;
+
+class WithdrawAudit extends Command
+{
+    protected function configure()
+    {
+        $this->setName('widthdrawAudit')
+            ->setDescription('津贴收益提现自动审核');
+    }
+
+    protected function execute(Input $input, Output $output)
+    { 
+        Capital::where('status',1)->where('type',2)->whereIn('log_type',[3,6])->where('end_time','<=',time())->update(['status'=>2]);
+        echo 'widthdrawAudit run success';
+    }
+
+}
