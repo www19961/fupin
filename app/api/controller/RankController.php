d3daec16334acf9cc34ef923a09e80c984f56b46
aaa <peterstonesooo@outlook>
2023-12-29 23:31:38 +0800

    ++

--- app/api/controller/RankController.php
+++ app/api/controller/RankController.php
@@ -8,7 +8,55 @@ use think\facade\Db;
 
 class RankController extends AuthController
 {
-    public function teamRankList()
+
+    
+    public function rankList()
+    {
+        $reward = [
+            1=>800,
+            2=>500,
+            3=>300,
+            4=>200,
+            5=>100,
+            6=>100,
+            7=>100,
+            8=>100,
+            9=>100,
+            10=>100,
+        ];
+        $relation = UserRelation::alias('r')->field(['count(r.sub_user_id) as team_num', 'r.user_id','phone'])->join('user u', 'u.id = r.sub_user_id')->where('r.is_active',1)->group('r.user_id')->order('team_num', 'desc')->limit(100)->select()->toArray();
+        $users = [];
+        //$rankData = Db::table('mp_active_rank')->field('phone,num as team_num')->select();
+
+        if (!empty($relation)) {
+/*             $relation = array_column($relation, 'team_num', 'user_id');
+
+            $user_ids = array_keys($relation);
+            array_unshift($user_ids, 'id');
+            $str = 'field('.implode(',', $user_ids).')';
+
+            $users = User::field('id,phone')->whereIn('id', $user_ids)->where('status', 1)->order(Db::raw($str))->select()->toArray();
+            foreach ($users as $k => $v) {
+                $users[$k]['phone'] = substr_replace($v['phone'],'****', 3, 4);
+                $users[$k]['team_num'] = $relation[$v['id']];
+            } */
+        }
+        foreach ($relation as $k => &$v) {
+            $v['phone'] = substr_replace($v['phone'],'****', 3, 4);
+            $v['sort'] = $k+1;
+            if($k<=10){
+                $v['reward'] = $reward[$k+1];
+            }else{
+                $v['reward'] = 20;
+            }
+        }
+/*         $column = array_column($relation,'team_num');
+        array_multisort($column,SORT_DESC,$users);
+        $data = array_slice($users,0,10); */
+        return out($relation);
+    }
+
+    public function teamRankList2()
     {
         $relation = UserRelation::alias('r')->field(['count(r.sub_user_id) as team_num', 'r.user_id'])->join('user u', 'u.id = r.sub_user_id')->where('r.is_active',1)->whereTime('r.created_at','today')->group('r.user_id')->order('team_num', 'desc')->limit(10)->select()->toArray();
         $users = [];
