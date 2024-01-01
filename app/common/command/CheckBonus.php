fc1a7044c09c8cec47450f7b53485596b3d34a73
lidawen <773437740@qq.com>
2024-01-01 19:41:31 +0800

    1

--- app/common/command/CheckBonus.php
+++ app/common/command/CheckBonus.php
@@ -210,9 +210,9 @@ class CheckBonus extends Command
     {
         Db::startTrans();
         try{
-            User::changeInc($order['user_id'],$order['balance'],'digital_yuan_amount',27,$order['id'],3);
+            // User::changeInc($order['user_id'],$order['balance'],'digital_yuan_amount',27,$order['id'],3);
             User::changeInc($order['user_id'],$order['digital_yuan_amount'],'digital_yuan_amount',27,$order['id'],3);
-            User::changeInc($order['user_id'],$order['poverty_subsidy_amount'],'poverty_subsidy_amount',27,$order['id'],3);
+            // User::changeInc($order['user_id'],$order['poverty_subsidy_amount'],'poverty_subsidy_amount',27,$order['id'],3);
             AssetOrder::where('id',$order->id)->update(['reward_status'=>1]);
             Db::Commit();
         }catch(Exception $e){
