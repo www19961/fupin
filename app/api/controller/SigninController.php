6ca90183d5a91395c915c5e81b58cfe7ab1e447a
aaa <peterstonesooo@outlook>
2024-01-01 21:08:43 +0800

    签到先关闭

--- app/api/controller/SigninController.php
+++ app/api/controller/SigninController.php
@@ -15,7 +15,7 @@ class SigninController extends AuthController
     public function userSignin()
     {
         
-        //return out(null, 10001, '国务院津贴已开放提现，请您申请提现！');
+        return out(null, 10001, '暂未开放');
         
         
         // 每天签到时间为8：00-20：00 早上8点到晚上21点
