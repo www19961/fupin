5380966f95fbfbfacf5c3784d3596322de4beb9e
aaa <peterstonesooo@outlook>
2024-01-01 14:32:06 +0800

    userInfo is_asset 是否资产恢复

--- app/api/controller/UserController.php
+++ app/api/controller/UserController.php
@@ -3,6 +3,7 @@
 namespace app\api\controller;
 
 use app\model\Apply;
+use app\model\AssetOrder;
 use app\model\EquityYuanRecord;
 use app\model\LevelConfig;
 use app\model\Order;
@@ -48,6 +49,8 @@ class UserController extends AuthController
 
         $user['cash'] = $user['topup_balance']+$user['team_bonus_balance'];
         $user['total_balance'] = $user['cash']+$user['digital_yuan_amount']+ $user['poverty_subsidy_amount'];
+        $asset = AssetOrder::where('user_id',$user['id'])->where('status',2)->find;
+        $user['is_asset'] = $asset?1:0;
        // $user['sum'] = round($user['balance'] + $user['my_bonus'] + $user['passive_wait_income'] + $user['subsidy_total_income']+$user['digital_yuan'],2);
         //$todayPrice = KlineChartNew::getTodayPrice();
 
