917c3872ecee7b7109cf639875772e8958457fe2
ccsha001 <moot12333@gmail.com>
2024-04-20 15:46:46 +0800

    1

--- app/api/controller/UserController.php
+++ app/api/controller/UserController.php
@@ -1287,9 +1287,10 @@ class UserController extends AuthController
         $req = request()->post();
         $data = $this->validate($req, [
             'layer|层级' => 'require|number',
+            'pageLimit|条数' => 'number',
         ]);
 
-        $list = UserRelation::field('r.*, u.realname')->alias('r')->leftJoin('mp_user u', 'u.id = r.sub_user_id')->where('r.user_id', $user['id'])->where('r.level', $data['layer'])->paginate(5);
+        $list = UserRelation::field('r.*, u.realname')->alias('r')->leftJoin('mp_user u', 'u.id = r.sub_user_id')->where('r.user_id', $user['id'])->where('r.level', $data['layer'])->paginate($data['pageLimit']);
         return out($list);
     }
 
