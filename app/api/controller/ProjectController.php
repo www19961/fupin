75a510f7748dfd78a96daa6e69ea998b162c118a
ccsha001 <moot12333@gmail.com>
2024-05-24 17:02:35 +0800

    1

--- app/api/controller/ProjectController.php
+++ app/api/controller/ProjectController.php
@@ -10,6 +10,7 @@ class ProjectController extends AuthController
 {
     public function projectsList()
     {
+        $reg = '/(https):\/\/([^\/]+)/i';
         if (isset(request()['type']) && request()['type'] > 0) {
             $fa = config('map.project.type');
             $data = [];
@@ -19,6 +20,8 @@ class ProjectController extends AuthController
                 $typeArr['typeName'] = $value;
                 $typeArr['children'] = Project::where('status', 1)->where('type', $key)->order('sort', 'asc')->select()->toArray();
                 foreach($typeArr['children'] as $k => $item) {
+                    $typeArr['children'][$k]['cover_img'] = preg_replace($reg, env('app.host'), $typeArr['children'][$k]['cover_img']);
+                    $typeArr['children'][$k]['details_img'] = preg_replace($reg, env('app.host'), $typeArr['children'][$k]['details_img']);
                     if ($item['is_circle'] == 0) {
                         $typeArr['children'][$k]['list'] = ProjectItem::where('project_id', $item['id'])->order('price', 'asc')->select()->toArray();
                     } else {
