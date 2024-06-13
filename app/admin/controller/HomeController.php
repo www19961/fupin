ac2db35c40f23ac0a9b12810908d311ee9864f05
ccsha001 <moot12333@gmail.com>
2024-06-14 00:00:24 +0800

    1

--- app/admin/controller/HomeController.php
+++ app/admin/controller/HomeController.php
@@ -17,10 +17,15 @@ class HomeController extends AuthController
         }
         $data = $arr = [];
 
-        $arr['title'] = '注册会员数';
-        $arr['value'] = User::count();
+        $today = date('Y-m-d 00:00:00');
+
+        $arr['title'] = '当日注册会员数';
+        $arr['value'] = User::where('created_at', '>', $today)->count();
+        $arr['title1'] = '注册总会员数';
+        $arr['value1'] = User::count();
         $arr['url'] = '';
         $data[] = $arr;
+        $arr = [];
 
         // $arr['title'] = '激活会员数';
         // $arr['value'] = User::where('is_active', 1)->count();
@@ -31,16 +36,23 @@ class HomeController extends AuthController
         $arr['value'] = round(Order::where('status', '>', 1)->sum('buy_amount'), 2);
         $arr['url'] = '';
         $data[] = $arr;
+        $arr = [];
 
-        $arr['title'] = '充值总金额';
-        $arr['value'] = round(Capital::where('status', 2)->where('type', 1)->sum('amount'), 2);
+        $arr['title'] = '当日充值总金额';
+        $arr['value'] = round(Capital::where('status', 2)->where('type', 1)->where('created_at', '>', $today)->sum('amount'), 2);
+        $arr['title1'] = '充值总金额';
+        $arr['value1'] = round(Capital::where('status', 2)->where('type', 1)->sum('amount'), 2);
         $arr['url'] = '';
         $data[] = $arr;
+        $arr = [];
 
-        $arr['title'] = '提现总金额';
-        $arr['value'] = round(0 - Capital::where('status', 2)->where('type', 2)->where('log_type',0)->sum('amount'), 2);
+        $arr['title'] = '当日提现总金额';
+        $arr['value'] = round(0 - Capital::where('status', 2)->where('type', 2)->where('log_type',0)->where('created_at', '>', $today)->sum('amount'), 2);
+        $arr['title1'] = '提现总金额';
+        $arr['value1'] = round(0 - Capital::where('status', 2)->where('type', 2)->where('log_type',0)->sum('amount'), 2);
         $arr['url'] = '';
         $data[] = $arr;
+        $arr = [];
 
         $arr['title'] = '充值总次数';
         $arr['value'] = Capital::where('status', 2)->where('type', 1)->where('log_type',0)->count();
@@ -51,11 +63,13 @@ class HomeController extends AuthController
         $arr['value'] = Capital::where('status', 2)->where('type', 2)->where('log_type',0)->count();
         $arr['url'] = '';
         $data[] = $arr;
+        $arr = [];
 
         $arr['title'] = '当日签到人数';
         $arr['value'] = UserSignin::where('signin_date', date('Y-m-d'))->count();
         $arr['url'] = '';
         $data[] = $arr;
+        $arr = [];
 
         $this->assign('data', $data);
 
